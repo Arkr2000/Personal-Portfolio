4d786d73b014052e81fd58037a419942d1908478
Arkr <anagharaj2000@gmail.com>
2022-08-18 11:52:55 +0530

    Adding modified files

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
     //Navbar on scrolling
 
     $(window).scroll(function (){
-        if ($(this).scrollTop()>10){
+        if ($(this).scrollTop()>200){
             $('.navbar').fadeIn('slow').css('display','flex');
         }else{
             $('.navbar').fadeOut('slow').css('display','none');
@@ -22,6 +22,59 @@
             smartBackspace: false,
             loop: true
         });
+
+    //Testimonial Carousel
+    4(".testimonial-carousel").owlCarousel({
+        autoplay:true,
+        smartSpeed: 1500,
+        dots:true,
+        loop:true,
+        items:1
+    });
+
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector:'.portfolio-item', 
+        layoutMode:'fitRows'
+    });
+    $('#portfolio-flters li').on('click',function(){
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter:$(this).data('filter')});
+
+    });
+    
+
+    // skill bar
+
+    $('.skill').waypoint(function(){
+        $('.progress .progress-bar').each(function(){
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+
     }
 })(jQuery);
 
+
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
